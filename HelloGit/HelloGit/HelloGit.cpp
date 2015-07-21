aa333fe7ddfc35b037813bb01ab80b1360626d26
Paul Liu(Beyondsoft Corporation) <v-paliu@microsoft.com>
2015-07-21 15:40:51 +0800

    Add source project to git

---
+++ HelloGit/HelloGit/HelloGit.cpp
@@ -0,0 +1,12 @@
+// HelloGit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello Git World!");
+	return 0;
+}
+
